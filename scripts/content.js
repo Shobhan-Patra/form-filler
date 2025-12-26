8f281d049255c510cb7f502cc0ada603808d71bc
Shobhan Patra <230170146055@vgecg.ac.in>
2025-12-26 20:00:46 +0530

    Refine field detection

--- scripts/content.js
+++ scripts/content.js
@@ -4,32 +4,48 @@ chrome.runtime.onMessage.addListener((request) => {
   }
 });
 
+function getLabel(element) {
+  const labels = document.getElementsByTagName("label");
+  for (let label of labels) {
+    if (label.htmlFor === element.id) {
+      return label.textContent.toLowerCase();
+    }
+  }
+  return "";
+}
+
+function checkField(field, keywords) {
+  const fieldName = (field?.name || "").toLowerCase();
+  const fieldValue = (field?.value || "").toLowerCase();
+  const fieldPlaceholder = (field?.placeholder || "").toLowerCase();
+  const fieldLabel = getLabel(field);
+
+  const fieldDetails = fieldName + fieldValue + fieldPlaceholder + fieldLabel;
+  return keywords.some(keyword => fieldDetails.includes(keyword));
+}
+
 function fillForm() {
-  const fields = { 
+  const savedFields = {
     name: "John Doe",
     email: "johndoe@example.com",
     phone: "9876543210",
     skills: "Javascript, Chrome Manifest, Git"
   };
 
+  const fieldKeywords = {
+    name: ["name", "first", "last", "full"],
+    email: ["email", "e-mail", "gmail", "g-mail"],
+    phone: ["mobile", "phone", "tel", "contact"],
+    skills: ["skills", "topics", "experience"]
+  };
+
   document.querySelectorAll("input").forEach(input => {
-    const name = (input.name || "").toLowerCase();
 
-    if (input.type === "text" && name.includes("name")) {
-      input.value = fields.name;
-    }
-
-    if (input.type === "email") {
-      input.value = fields.email;
-    }
-
-    if (input.type === "tel") {
-      input.value = fields.phone;
-    }
-
-    if (input.type === "text" && name.includes("skill")) {
-      input.value = fields.skills;
-    }
+    ["name", "email", "phone", "skills"].forEach(field => {
+      if (checkField(input, fieldKeywords[field])) {
+        input.value = savedFields[field];
+      }
+    })
 
     input.dispatchEvent(new Event("input", { bubbles: true }));
     input.dispatchEvent(new Event("change", { bubbles: true }));
